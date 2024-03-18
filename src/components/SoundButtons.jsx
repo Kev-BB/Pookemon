a3a36e15e67c281a4684c27ccfa3f3e09ed035a7
Kev-BB <118001660+Kev-BB@users.noreply.github.com>
2024-03-18 17:48:11 +0300

    Fixed Sound Issue

--- src/components/SoundButtons.jsx
+++ src/components/SoundButtons.jsx
@@ -1,17 +1,21 @@
 export default function SoundButtons({ clickHandlers }) {
   return (
-    <div className='start-and-select-buttons-container'>
-      <div className='select-button'>
-        <button onClick={clickHandlers.turnOffSound}></button>
+    <div className="start-and-select-buttons-container">
+      <div className="select-button">
+        <button onClick={clickHandlers.turnOffSound}>
+          {" "}
+          <img src="./assets/images/icons/sound-off.svg" />
+        </button>
         <span>Seçiniz</span>
-        <img src='./assets/images/icons/sound-off.svg' />
       </div>
 
-      <div className='start-button'>
-        <button onClick={clickHandlers.turnOnSound}></button>
+      <div className="start-button">
+        <button onClick={clickHandlers.turnOnSound}>
+          {" "}
+          <img src="./assets/images/icons/sound-on.svg" />
+        </button>
         <span>Başlat</span>
-        <img src='./assets/images/icons/sound-on.svg' />
       </div>
     </div>
-  )
+  );
 }
